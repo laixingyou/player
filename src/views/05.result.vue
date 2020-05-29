e59337c9b48fe08aba8730776b6306d06cc19dfc
laixingyou <1156663717@qq.com>
2020-05-29 22:53:06 +0800

    1

--- src/views/05.result.vue
+++ src/views/05.result.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="result-container">
     <div class="title-wrap">
-      <h2 class="title">{{$route.query.q}}</h2>
-      <span class="sub-title">找到985个结果</span>
+      <h2 class="title">{{ $route.query.q }}</h2>
+      <span class="sub-title">找到 {{ count }} 个结果</span>
     </div>
     <el-tabs v-model="activeIndex">
       <el-tab-pane label="歌曲" name="songs">
@@ -15,172 +15,69 @@
             <th>时长</th>
           </thead>
           <tbody>
-            <tr class="el-table__row">
+            <tr
+              v-for="(item, index) in songList"
+              :key="index"
+              class="el-table__row"
+              @dblclick="playMusic(item.id)"
+            >
               <td>1</td>
               <td>
                 <div class="song-wrap">
                   <div class="name-wrap">
-                    <span>你要相信这不是最后一天</span>
-                    <span class="iconfont icon-mv"></span>
+                    <span>{{ item.name }}</span>
+                    <span v-if="item.mvid != 0" class="iconfont icon-mv"></span>
                   </div>
-                  <span>电视剧加油练习生插曲</span>
+                  <span v-if="item.alias.length != 0">{{ item.alias[0] }}</span>
                 </div>
               </td>
-              <td>华晨宇</td>
-              <td>你要相信这不是最后一天</td>
-              <td>06:03</td>
-            </tr>
-            <tr class="el-table__row">
-              <td>2</td>
-              <td>
-                <div class="song-wrap">
-                  <div class="name-wrap">
-                    <span>你要相信这不是最后一天</span>
-                    <span class="iconfont icon-mv"></span>
-                  </div>
-                </div>
-              </td>
-              <td>华晨宇</td>
-              <td>你要相信这不是最后一天</td>
-              <td>06:03</td>
+              <td>{{ item.artists[0].name }}</td>
+              <td>{{ item.album.name }}</td>
+              <td>{{ item.duration }}</td>
             </tr>
           </tbody>
         </table>
       </el-tab-pane>
       <el-tab-pane label="歌单" name="lists">
         <div class="items">
-          <div class="item">
+          <div
+            v-for="(item, index) in playlists"
+            :key="index"
+            class="item"
+            @click="toPlaylist(item.id)"
+          >
             <div class="img-wrap">
               <div class="num-wrap">
                 播放量:
-                <span class="num">66892</span>
+                <span class="num">{{ item.playCount }}</span>
               </div>
-              <img src="../assets/cover.jpg" alt />
+              <img :src="item.coverImgUrl" alt="" />
               <span class="iconfont icon-play"></span>
             </div>
-            <p class="name">编辑推荐：一起探索这个未知的音乐罐头吧！</p>
-          </div>
-          <div class="item">
-            <div class="img-wrap">
-              <div class="num-wrap">
-                播放量:
-                <span class="num">66892</span>
-              </div>
-              <img src="../assets/cover.jpg" alt />
-              <span class="iconfont icon-play"></span>
-            </div>
-            <p class="name">编辑推荐：一起探索这个未知的音乐罐头吧！</p>
-          </div>
-          <div class="item">
-            <div class="img-wrap">
-              <div class="num-wrap">
-                播放量:
-                <span class="num">66892</span>
-              </div>
-              <img src="../assets/cover.jpg" alt />
-              <span class="iconfont icon-play"></span>
-            </div>
-            <p class="name">编辑推荐：一起探索这个未知的音乐罐头吧！</p>
-          </div>
-          <div class="item">
-            <div class="img-wrap">
-              <div class="num-wrap">
-                播放量:
-                <span class="num">66892</span>
-              </div>
-              <img src="../assets/cover.jpg" alt />
-              <span class="iconfont icon-play"></span>
-            </div>
-            <p class="name">编辑推荐：一起探索这个未知的音乐罐头吧！</p>
-          </div>
-          <div class="item">
-            <div class="img-wrap">
-              <div class="num-wrap">
-                播放量:
-                <span class="num">66892</span>
-              </div>
-              <img src="../assets/cover.jpg" alt />
-              <span class="iconfont icon-play"></span>
-            </div>
-            <p class="name">编辑推荐：一起探索这个未知的音乐罐头吧！</p>
+            <p class="name">{{ item.name }}</p>
           </div>
         </div>
       </el-tab-pane>
       <el-tab-pane label="MV" name="mv">
         <div class="items mv">
-          <div class="item">
+          <div
+            v-for="(item, index) in mv"
+            :key="index"
+            class="item"
+            @click="toMV(item.id)"
+          >
             <div class="img-wrap">
-              <img src="../assets/mvCover.jpg" alt />
+              <img :src="item.cover" alt="" />
               <span class="iconfont icon-play"></span>
               <div class="num-wrap">
                 <div class="iconfont icon-play"></div>
-                <div class="num">9912</div>
+                <div class="num">{{ item.playCount }}</div>
               </div>
-              <span class="time">02:43</span>
+              <span class="time">{{ item.duration }}</span>
             </div>
             <div class="info-wrap">
-              <div class="name">HEYNA</div>
-              <div class="singer">余恩</div>
-            </div>
-          </div>
-          <div class="item">
-            <div class="img-wrap">
-              <img src="../assets/mvCover.jpg" alt />
-              <span class="iconfont icon-play"></span>
-              <div class="num-wrap">
-                <div class="iconfont icon-play"></div>
-                <div class="num">9912</div>
-              </div>
-              <span class="time">02:43</span>
-            </div>
-            <div class="info-wrap">
-              <div class="name">HEYNA</div>
-              <div class="singer">余恩</div>
-            </div>
-          </div>
-          <div class="item">
-            <div class="img-wrap">
-              <img src="../assets/mvCover.jpg" alt />
-              <span class="iconfont icon-play"></span>
-              <div class="num-wrap">
-                <div class="iconfont icon-play"></div>
-                <div class="num">9912</div>
-              </div>
-              <span class="time">02:43</span>
-            </div>
-            <div class="info-wrap">
-              <div class="name">HEYNA</div>
-              <div class="singer">余恩</div>
-            </div>
-          </div>
-          <div class="item">
-            <div class="img-wrap">
-              <img src="../assets/mvCover.jpg" alt />
-              <span class="iconfont icon-play"></span>
-              <div class="num-wrap">
-                <div class="iconfont icon-play"></div>
-                <div class="num">9912</div>
-              </div>
-              <span class="time">02:43</span>
-            </div>
-            <div class="info-wrap">
-              <div class="name">HEYNA</div>
-              <div class="singer">余恩</div>
-            </div>
-          </div>
-          <div class="item">
-            <div class="img-wrap">
-              <img src="../assets/mvCover.jpg" alt />
-              <span class="iconfont icon-play"></span>
-              <div class="num-wrap">
-                <div class="iconfont icon-play"></div>
-                <div class="num">9912</div>
-              </div>
-              <span class="time">02:43</span>
-            </div>
-            <div class="info-wrap">
-              <div class="name">HEYNA</div>
-              <div class="singer">余恩</div>
+              <div class="name">{{ item.name }}</div>
+              <div class="singer">{{ item.artistName }}</div>
             </div>
           </div>
         </div>
@@ -190,15 +87,162 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "result",
   data() {
     return {
-      activeIndex: "songs"
+      // tab切换时 会改变的数据
+      activeIndex: "songs",
+      songList: [],
+      playlists: [],
+      mv: [],
+      count: 0,
     };
-  }
+  },
+  created() {
+    axios({
+      url: "https://autumnfish.cn/search",
+      method: "get",
+      params: {
+        keywords: this.$route.query.q,
+        type: 1,
+        limit: 10,
+      },
+    }).then((res) => {
+      console.log(res);
+      this.songList = res.data.result.songs;
+      // 计算歌曲时间
+      for (let i = 0; i < this.songList.length; i++) {
+        let min = parseInt(this.songList[i].duration / 1000 / 60);
+        let sec = parseInt((this.songList[i].duration / 1000) % 60);
+        if (min < 10) {
+          min = "0" + min;
+        }
+        if (sec < 10) {
+          sec = "0" + sec;
+        }
+        // console.log(min + '|' + sec)
+        this.songList[i].duration = min + ":" + sec;
+      }
+      this.count = res.data.result.songCount;
+    });
+  },
+  // 侦听器
+  watch: {
+    activeIndex() {
+      let type = 1;
+
+      // 获取个数
+      let limit = 10;
+      switch (this.activeIndex) {
+        case "songs":
+          type = 1;
+          limit = 10;
+          break;
+        case "lists":
+          type = 1000;
+          limit = 10;
+          break;
+        case "mv":
+          type = 1004;
+          limit = 8;
+          break;
+
+        default:
+          break;
+      }
+
+      axios({
+        url: "https://autumnfish.cn/search",
+        method: "get",
+        params: {
+          keywords: this.$route.query.q,
+          type,
+          // 获取的数据量
+          limit,
+        },
+      }).then((res) => {
+        console.log(res);
+
+        if (type == 1) {
+          // 歌曲
+          this.songList = res.data.result.songs;
+          // 计算歌曲时间
+          for (let i = 0; i < this.songList.length; i++) {
+            let min = parseInt(this.songList[i].duration / 1000 / 60);
+            let sec = parseInt((this.songList[i].duration / 1000) % 60);
+            if (min < 10) {
+              min = "0" + min;
+            }
+            if (sec < 10) {
+              sec = "0" + sec;
+            }
+            // console.log(min + '|' + sec)
+            this.songList[i].duration = min + ":" + sec;
+          }
+
+          this.count = res.data.result.songCount;
+        }
+        // 获取 歌单
+        else if (type == 1000) {
+          this.playlists = res.data.result.playlists;
+          this.count = res.data.result.playlistCount;
+          // 处理 播放次数
+          for (let i = 0; i < this.playlists.length; i++) {
+            if (this.playlists[i].playCount > 100000) {
+              this.playlists[i].playCount =
+                parseInt(this.playlists[i].playCount / 10000) + "万";
+            }
+          }
+        } else {
+          this.mv = res.data.result.mvs;
+          this.count = res.data.result.mvCount;
+          // 处理数据
+          for (let i = 0; i < this.mv.length; i++) {
+            // 时间
+            let min = parseInt(this.mv[i].duration / 1000 / 60);
+            let sec = parseInt((this.mv[i].duration / 1000) % 60);
+            if (min < 10) {
+              min = "0" + min;
+            }
+            if (sec < 10) {
+              sec = "0" + sec;
+            }
+            this.mv[i].duration = min + ":" + sec;
+
+            if (this.mv[i].playCount > 100000) {
+              this.mv[i].playCount =
+                parseInt(this.mv[i].playCount / 10000) + "万";
+            }
+          }
+        }
+      });
+    },
+  },
+  methods: {
+    toMV(id) {
+      this.$router.push(`/mv?q=${id}`);
+    },
+    toPlaylist(id) {
+      this.$router.push(`/playlist?q=${id}`);
+    },
+    playMusic(id) {
+      axios({
+        url: "https://autumnfish.cn/song/url",
+        method: "get",
+        params: {
+          id, // id:id
+        },
+      }).then((res) => {
+        // console.log(res)
+        let url = res.data.data[0].url;
+        // console.log(this.$parent.musicUrl)
+        this.$parent.musicUrl = url;
+      });
+    },
+  },
 };
 </script>
 
-<style >
-</style>
+<style></style>
